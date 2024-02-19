90f83ec6998b615ce5c7fbd6d8532e13bbe98927
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2024-02-20 00:12:48 +0100

    vercel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 
-const characterRouter = require('./routes/character.routes');
+const boxeadoresRouter = require('./routes/boxeadores.routes.js');
 const  connect = require('./utils/db/connect.js');
 
 connect();
@@ -8,7 +9,19 @@ connect();
 const PORT = 3000;
 const server = express();
 
-server.use('/character', characterRouter);
+// Para usar POSTMAN. instalar la dependencia cors npm install --save cors, corsOtions    
+server.use(cors());
+// Parsea Post y Put que vienen como JSON.
+server.use(express.json());
+// Parsea Post y Put que vienen como STRING o ARRAY.
+server.use(express.urlencoded({ extended: false }));
+
+// Mensage para la ruta vacia de vercel.
+server.get('/', (req,res) => {
+    res.json("Bienvenido a mi API Jonatan!");
+});
+
+server.use('/boxeadores', boxeadoresRouter);
 
 server.listen(PORT, () => {
     console.log(`El servidor esta escuchado en http://localhost:${PORT}`);
---
+++ routes/boxeadores.routes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Boxeadores = require('../models/Boxeadores.js');
+
+const boxeadoresRouter = express.Router();
+
+boxeadoresRouter.get('/', async (req, res, next) => {
+    try {
+        const boxeadores = await Boxeadores.find();
+        return res.status(200).json(boxeadores)
+    } catch(err) {
+        next(err);
+    }
+});
+
+
+boxeadoresRouter.get('/:id',  async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const boxeadores = await Boxeadores.findById(id);
+        if (boxeadores) {
+            return res.status(200).json(boxeadores);
+        } else {
+            next(createError('No existe el boxeador con el id indicado', 404));
+        }
+    } catch (err) {
+        next(err);
+    }
+  });
+
+
+boxeadoresRouter.post('/', async (req, res, next) => {
+    try {
+        const newBoxeador = new Boxeadores({...req.body});
+        const createdBoxeadores = await newBoxeador.save();
+        return res.status(201).json(createdBoxeadores);
+    } catch (err) {
+        next(err);
+    }
+});
+
+boxeadoresRouter.put('/:id', async (req, res, next) => {
+    try {
+       const id = req.params.id;
+       const modifiedBoxeadores = new Boxeadores({...req.body});
+       //Para que no genere un id aleatorio y lo deje como fijo.
+       modifiedBoxeadores.id = id;
+       // Para actualizar, Pero no me cambia los datos de la movie.
+       const boxeadoresUpdate = await Boxeadores.findByIdAndUpdate(
+          id,
+          modifiedBoxeadores,
+          //Añado new = true para que me traiga la movie con los cambios realizados.
+          {new: true}
+       );
+       // Por ultimo el estatus json + paramatro
+       return res.status(200).json(boxeadoresUpdate);
+    }catch (err) {
+       next(err);
+    }
+});
+
+boxeadoresRouter.delete('/:id', async (req, res, next) => {
+    try{  
+       const id = req.params.id;
+       await Boxeadores.findByIdAndDelete(id);
+       return res.status(200).json('El boxeador se a eliminado correctamente.')
+    } catch(err) {
+       next(err);
+    }
+  });
+
+module.exports = boxeadoresRouter;
